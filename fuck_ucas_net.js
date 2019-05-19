abfc1324738a91aaf93fb77a2dabe8557a297576
idejie <i@idejie.com>
2019-05-19 21:18:20 +0800

    Create fuck_ucas_net.js

---
+++ fuck_ucas_net.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name         国科大上网脚本
+// @namespace    https://github.com/idejie/Scripts/blob/master/fuck_ucas_net.js
+// @version      2019.05.18
+// @description  A script for F**king UCAS network
+// @author       idejie
+// @match        http://210.77.16.21/eportal/index.jsp*
+// @supportURL   https://github.com/idejie
+// @date         2019/05/19
+// @modified    2019/05.19
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    // Your code here...
+    checkForm=()=>true;
+})();
